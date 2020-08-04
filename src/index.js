1514a82626cff33149119fad0a652b133cc8abbc
Silesio-Cipriano <68440141+Silesio-Cipriano@users.noreply.github.com>
2020-08-04 11:14:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import Spaceship from "./Spaceship";
-import armament from "./armament";
-import { laserDefense, steelDefense } from "./defenses";
 
-let spaceship = new Spaceship("Dhart", "Silésio", armament.laser, steelDefense);
-
-console.log(spaceship);
